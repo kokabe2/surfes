512401e1f3c6e4309b9f7cc3ca2c6ad8954c4ae0
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-05-07 22:41:43 +0900

    Add IoData module

---
+++ boot/boot/src/infrastructures/drivers/io_data.c
@@ -0,0 +1,24 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "io_data.h"
+
+void IoData_Write8bit(ioAddress offset, uint8_t data) {
+  uint8_t volatile *address = (uint8_t *)offset;
+  *address = data;
+}
+
+void IoData_Write16bit(ioAddress offset, uint16_t data) {
+  uint16_t volatile *address = (uint16_t *)offset;
+  *address = data;
+}
+
+void IoData_Write32bit(ioAddress offset, uint32_t data) {
+  uint32_t volatile *address = (uint32_t *)offset;
+  *address = data;
+}
+
+uint8_t IoData_Read8bit(ioAddress offset) { return *(uint8_t *)offset; }
+
+uint16_t IoData_Read16bit(ioAddress offset) { return *(uint16_t *)offset; }
+
+uint32_t IoData_Read32bit(ioAddress offset) { return *(uint32_t *)offset; }
