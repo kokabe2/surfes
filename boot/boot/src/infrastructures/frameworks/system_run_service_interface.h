cffa79be2be8ba0fdf6dda4651fb85e6bceffd62
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-05-08 23:07:03 +0900

    Add SystemRunner module

---
+++ boot/boot/src/infrastructures/frameworks/system_run_service_interface.h
@@ -0,0 +1,9 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef BOOT_BOOT_SRC_INFRASTRUCTURES_FRAMEWORKS_SYSTEM_RUN_SERVICE_INTERFACE_H_
+#define BOOT_BOOT_SRC_INFRASTRUCTURES_FRAMEWORKS_SYSTEM_RUN_SERVICE_INTERFACE_H_
+
+void SystemRunServiceInterface_Halt(void);
+void SystemRunServiceInterface_Reboot(void);
+
+#endif  // BOOT_BOOT_SRC_INFRASTRUCTURES_FRAMEWORKS_SYSTEM_RUN_SERVICE_INTERFACE_H_
---
+++ boot/boot/src/infrastructures/frameworks/system_runner.h
@@ -0,0 +1,10 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef BOOT_BOOT_SRC_INFRASTRUCTURES_FRAMEWORKS_SYSTEM_RUNNER_H_
+#define BOOT_BOOT_SRC_INFRASTRUCTURES_FRAMEWORKS_SYSTEM_RUNNER_H_
+
+void SystemRunner_Create(int defalut_runlevel);
+void SystemRunner_Destroy(void);
+void SystemRunner_Run(void);
+
+#endif  // BOOT_BOOT_SRC_INFRASTRUCTURES_FRAMEWORKS_SYSTEM_RUNNER_H_
