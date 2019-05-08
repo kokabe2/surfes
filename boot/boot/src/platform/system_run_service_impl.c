0497a20a26d52f20f2aa5419644f0e462076b373
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-05-08 23:09:59 +0900

    Add SystemRunServiceImpl module

---
+++ boot/boot/src/platform/system_run_service_impl.c
@@ -0,0 +1,11 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "infrastructures/frameworks/system_run_service_interface.h"
+
+#include "microcontroller.h"
+
+void SystemRunServiceInterface_Halt(void) {
+  for (;;) continue;
+}
+
+void SystemRunServiceInterface_Reboot(void) { Microcontroller_Reset(); }
