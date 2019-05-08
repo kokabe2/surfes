ce3b9ba9d32c9c848ff9f35351feb8863a1eb0e0
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-05-08 23:08:00 +0900

    Add MicroController module

---
+++ boot/boot/src/platform/microcontroller.h
@@ -0,0 +1,9 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef BOOT_BOOT_SRC_PLATFORM_MICROCONTROLLER_H_
+#define BOOT_BOOT_SRC_PLATFORM_MICROCONTROLLER_H_
+
+void Microcontroller_Initialize(void);
+void Microcontroller_Reset(void);
+
+#endif  // BOOT_BOOT_SRC_PLATFORM_MICROCONTROLLER_H_
