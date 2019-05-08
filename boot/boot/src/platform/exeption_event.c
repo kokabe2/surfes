fd2ebbd9b00e91f47c327f66f1c58d69ed218c53
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-05-08 23:09:33 +0900

    Add ExeptionEvent module

---
+++ boot/boot/src/platform/exeption_event.c
@@ -0,0 +1,7 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "exeption_event.h"
+
+void ExeptionEvent_Initialize(void) {
+  // do something
+}
---
+++ boot/boot/src/platform/exeption_event.h
@@ -0,0 +1,8 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef BOOT_BOOT_SRC_PLATFORM_EXEPTION_EVENT_H_
+#define BOOT_BOOT_SRC_PLATFORM_EXEPTION_EVENT_H_
+
+void ExeptionEvent_Initialize(void);
+
+#endif  // BOOT_BOOT_SRC_PLATFORM_EXEPTION_EVENT_H_
