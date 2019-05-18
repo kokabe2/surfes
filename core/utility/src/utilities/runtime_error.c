ce17b1f80ad787f3b6a37644d64151c9056dd09f
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-05-19 00:33:33 +0900

    Remove Utility package from Core directory

--- core/utility/src/utilities/runtime_error.c
+++
@@ -1,8 +0,0 @@
-﻿// Copyright(c) 2019 Ken Okabe
-// This software is released under the MIT License, see LICENSE.
-#include "runtime_error.h"
-
-void RuntimeError(const char *message, int paramter, const char *file,
-                  int line) {
-  // do something
-}
