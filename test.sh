5fedd0884b3604c55efdc57c570ce0eefaa678fb
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-05-19 00:53:53 +0900

    Update .travis.yml

---
+++ test.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/sh
+
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+
+test_package () {
+    mkdir -p build
+    cd build
+    cmake ..
+    make
+    ctest
+}
+
+cd ${SCRIPT_DIR}/boot/boot && test_package
+cd ${SCRIPT_DIR}/utility && test_package
