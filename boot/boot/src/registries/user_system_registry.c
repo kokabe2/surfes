6c799d9404c63c1c6f1a4adb3ff237059471a8b9
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-05-19 23:49:47 +0900

    Add UserSystemRegistry module

---
+++ boot/boot/src/registries/user_system_registry.c
@@ -0,0 +1,18 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "user_system_registry.h"
+
+#include <stddef.h>
+
+#include "user_system_registry_file.h"
+#include "user_system_registry_file_interface.h"
+
+static uintptr_t its_address = kUserSystemRegistryFileAddress;
+void UserSystemRegistry_Change(uintptr_t address) { its_address = address; }
+
+static UserSystemRegistryFile its_file = NULL;
+ISystemExecutable UserSystemRegistry_getExecutor(int runlevel) {
+  if (!its_file) its_file = UserSystemRegistryFile_Open(its_address);
+
+  return UserSystemRegistryFile_getExecutor(its_file, runlevel);
+}
---
+++ boot/boot/src/registries/user_system_registry.h
@@ -0,0 +1,13 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef BOOT_BOOT_SRC_REGISTRIES_USER_SYSTEM_REGISTRY_H_
+#define BOOT_BOOT_SRC_REGISTRIES_USER_SYSTEM_REGISTRY_H_
+
+#include <stdint.h>
+
+#include "system_executable.h"
+
+void UserSystemRegistry_Change(uintptr_t address);
+ISystemExecutable UserSystemRegistry_getExecutor(int runlevel);
+
+#endif  // BOOT_BOOT_SRC_REGISTRIES_USER_SYSTEM_REGISTRY_H_
