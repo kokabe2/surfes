bec87425579ac75022123d58062221962ef727f1
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-05-22 22:47:39 +0900

    Add Boot file version to fixed registry data

--- boot/registry/include/registry_fixed_api.h
+++ boot/registry/include/registry_fixed_api.h
@@ -3,7 +3,8 @@
 #ifndef BOOT_REGISTRY_INCLUDE_REGISTRY_FIXED_API_H_
 #define BOOT_REGISTRY_INCLUDE_REGISTRY_FIXED_API_H_
 
-enum BootFileAddress {
+enum BootFileMetadata {
+  kBootFileVersion = 0x0000000100000000,
   kBootFileAddress = 0xFFFFE000,
   kRegistryFileAddress = 0xFFFFC000,
 };
