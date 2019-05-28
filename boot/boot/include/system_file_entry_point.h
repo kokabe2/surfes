6c7fefb2daea8dd91e8df58bf76cffd619a82869
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-05-28 22:31:31 +0900

    Add SystemFileEntryPoint interface

---
+++ boot/boot/include/system_file_entry_point.h
@@ -0,0 +1,10 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef BOOT_BOOT_INCLUDE_SYSTEM_FILE_ENTRY_POINT_H_
+#define BOOT_BOOT_INCLUDE_SYSTEM_FILE_ENTRY_POINT_H_
+
+typedef struct SystemFileEntryPointStruct {
+  int (*Execute)(int runlevel);  // Returns next runlevel
+} SystemFileEntryPointStruct, *SystemFileEntryPoint;
+
+#endif  // BOOT_BOOT_INCLUDE_SYSTEM_FILE_ENTRY_POINT_H_
