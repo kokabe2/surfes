f5a33ac44f7aa7e684ab0aacc0d22ac1beda1d5a
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-06-03 22:56:41 +0900

    Rename kUpdaterFileAddress -> kRecoveryFileAddress

--- boot/registry/include/immutable_registry.h
+++ boot/registry/include/immutable_registry.h
@@ -1,4 +1,4 @@
-﻿// Copyright(c) 2019 Ken Okabe
+// Copyright(c) 2019 Ken Okabe
 // This software is released under the MIT License, see LICENSE.
 #ifndef BOOT_REGISTRY_INCLUDE_IMMUTABLE_REGISTRY_H_
 #define BOOT_REGISTRY_INCLUDE_IMMUTABLE_REGISTRY_H_
@@ -7,7 +7,7 @@ enum BootFileMetadata {
   kBootFileVersion = 0x0000000100000000,
   kBootFileAddress = 0xFFFFE000,
   kRegistryFileAddress = 0xFFFFC000,
-  kUpdaterFileAddress = 0xFFFF0000,
+  kRecoveryFileAddress = 0xFFFF0000,
 };
 
 enum Runlevel {
