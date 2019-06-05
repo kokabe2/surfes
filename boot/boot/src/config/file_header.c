6712b5afa6574b7798736fb6b19c86e9acd78d4c
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-06-06 00:31:46 +0900

    Use enumerator instead of magic number

--- boot/boot/src/config/file_header.c
+++ boot/boot/src/config/file_header.c
@@ -2,13 +2,14 @@
 // This software is released under the MIT License, see LICENSE.
 #include "sif_header.h"
 
+#include "immutable_registry.h"
+
 const SifHeaderStruct kFileHeader = {
     .identification = {0x7F, 'S', 'I', 'F', kSc32, kSd2Lsb, kSvCurrent},
     .type = kStExe,
     .machine = kSmRx,
     .header_size = sizeof(SifHeaderStruct),
-    .file_version = 0x0000000100000000,
-    .file_size = 8 * 1024,
-    .file_address = 0xFFFFE000,
+    .file_version = kBootFileVersion,
+    .file_address = kBootFileAddress,
     .entry_point = 0xFFFFFFFC,
 };
