ffd9f676fc1766a83e73f876619f8c5fdd814f82
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-06-06 22:11:03 +0900

    Fix include guards

--- utility/include/spy_runtime_error.h
+++ utility/include/spy_runtime_error.h
@@ -1,10 +1,10 @@
 ﻿// Copyright(c) 2019 Ken Okabe
 // This software is released under the MIT License, see LICENSE.
-#ifndef UTILITY_TEST_INFRASTRUCTURES_SPY_RUNTIME_ERROR_H_
-#define UTILITY_TEST_INFRASTRUCTURES_SPY_RUNTIME_ERROR_H_
+#ifndef UTILITY_INCLUDE_SPY_RUNTIME_ERROR_H_
+#define UTILITY_INCLUDE_SPY_RUNTIME_ERROR_H_
 
 void SpyRuntimeError_Reset(void);
 const char* SpyRuntimeError_GetLastError(void);
 int SpyRuntimeError_GetLastParameter(void);
 
-#endif  // UTILITY_TEST_INFRASTRUCTURES_SPY_RUNTIME_ERROR_H_
+#endif  // UTILITY_INCLUDE_SPY_RUNTIME_ERROR_H_
