a234d3cf3092c67f308386774e4ec720f6c365b9
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-06-09 22:26:41 +0900

    Add boot component for updater file

---
+++ updater/boot/src/bootstrapper.c
@@ -0,0 +1,9 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "system_file_entry_point.h"
+
+int BootStrapper(int runlevel) { return 0; }
+
+const SystemFileEntryPointStruct kSystemFileEntryPoint = {
+    .Execute = BootStrapper,
+};
