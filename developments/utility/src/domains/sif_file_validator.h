c85ea08b37759476a59a1b1dbd51df7f07eb1360
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-08-24 21:40:30 +0900

    Fix the symbol name for #ifndef header guard

--- developments/utility/src/domains/sif_file_validator.h
+++ developments/utility/src/domains/sif_file_validator.h
@@ -1,7 +1,7 @@
 ﻿// Copyright(c) 2019 Ken Okabe
 // This software is released under the MIT License, see LICENSE.
-#ifndef UTILITY_SRC_DOMAINS_SIF_FILE_VALIDATOR_H_
-#define UTILITY_SRC_DOMAINS_SIF_FILE_VALIDATOR_H_
+#ifndef DEVELOPMENTS_UTILITY_SRC_DOMAINS_SIF_FILE_VALIDATOR_H_
+#define DEVELOPMENTS_UTILITY_SRC_DOMAINS_SIF_FILE_VALIDATOR_H_
 
 #include <stdint.h>
 
@@ -17,4 +17,4 @@ enum SifFileValidationError {
 
 int SifFileValidator_Validate(uintptr_t file_address);
 
-#endif  // UTILITY_SRC_DOMAINS_SIF_FILE_VALIDATOR_H_
+#endif  // DEVELOPMENTS_UTILITY_SRC_DOMAINS_SIF_FILE_VALIDATOR_H_
