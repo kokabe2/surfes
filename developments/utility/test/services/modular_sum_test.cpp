0e7b1daa8ca4909dc2203328d75800ea02fcb7c8
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-08-24 12:24:36 +0900

    Move dummy_data[] to member variable of ModularSumTest class

--- developments/utility/test/services/modular_sum_test.cpp
+++ developments/utility/test/services/modular_sum_test.cpp
@@ -6,55 +6,44 @@ extern "C" {
 #include "modular_sum.h"
 }
 
-class ModularSumTest : public ::testing::Test {};
-
-TEST_F(ModularSumTest, Verify) {
+class ModularSumTest : public ::testing::Test {
+ protected:
   uint32_t dummy_data[16]{
       0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
   };
+};
 
+TEST_F(ModularSumTest, Verify) {
   uint32_t sum = ModularSum_Verify(dummy_data, sizeof(dummy_data));
 
   EXPECT_EQ(120, sum);
 }
 
 TEST_F(ModularSumTest, VerifyWithSizeLessThanOne) {
-  uint32_t dummy_data[16]{0};
-
   uint32_t sum = ModularSum_Verify(dummy_data, 0);
 
   EXPECT_NE(0, sum);
 }
 
 TEST_F(ModularSumTest, VerifyWithSizeNotMultiplesOfFour) {
-  uint32_t dummy_data[16]{0};
-
   uint32_t sum = ModularSum_Verify(dummy_data, sizeof(dummy_data) - 1);
 
   EXPECT_NE(0, sum);
 }
 
 TEST_F(ModularSumTest, Calculate) {
-  uint32_t dummy_data[16]{
-      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-  };
-
   uint32_t checksum = ModularSum_Calculate(dummy_data, sizeof(dummy_data));
 
   EXPECT_EQ(0xFFFFFF88, checksum);
 }
 
 TEST_F(ModularSumTest, CalculateWithSizeLessThanOne) {
-  uint32_t dummy_data[16]{1};
-
   uint32_t checksum = ModularSum_Calculate(dummy_data, 0);
 
   EXPECT_EQ(0, checksum);
 }
 
 TEST_F(ModularSumTest, CalculateWithSizeNotMultiplesOfFour) {
-  uint32_t dummy_data[16]{1};
-
   uint32_t checksum = ModularSum_Calculate(dummy_data, sizeof(dummy_data) - 1);
 
   EXPECT_EQ(0, checksum);
